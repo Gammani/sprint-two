03985dc29667d19b8abeb0ba78ed2c4b12c45f8e
Oreda-Z <shtucer31@gmail.com>
2022-11-06 17:04:19 +0300

    fix

--- src/middlewares/requestValidatorWithExpressValidator.ts
+++ src/middlewares/requestValidatorWithExpressValidator.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from "express";
 import {CustomValidator, validationResult} from "express-validator";
 import {ErrorsType} from "../utils/types";
-import {bloggers} from "../repositories/bloggers-repository";
+import {bloggers, bloggersRepository} from "../repositories/bloggers-repository";
 
 
 export const checkedValidation = (req: Request, res: Response, next: NextFunction) => {
@@ -18,8 +18,9 @@ export const checkedValidation = (req: Request, res: Response, next: NextFunctio
 
 export const isValidId: CustomValidator = blogId => {
     const foundBloggers = bloggers.find(b => b.id === blogId)
-    if(foundBloggers) {
+    if(!foundBloggers) {
         return Promise.reject('blogId не валидный')
+    } else {
+        return true
     }
-    return
 }
