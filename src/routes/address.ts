e4a8aac2ce0634ace03c08dbf6ebc3f5564f3a6d
Oreda-Z <shtucer31@gmail.com>
2023-05-12 23:43:21 +0300

    fix

--- src/routes/address.ts
+++ src/routes/address.ts
@@ -3,6 +3,6 @@ import {Router, Request, Response} from "express";
 export const addressRouter = Router({})
 
 addressRouter.get('/', (req: Request, res: Response) => {
-    const ipAdr = req.socket.remoteAddress
+    const ipAdr = req.socket.address()
     res.send(ipAdr)
 })
