b5f92331a79b676977e82ea120e0f1437229ebbb
Oreda-Z <shtucer31@gmail.com>
2023-05-13 17:12:11 +0300

    fix

--- src/routes/address.ts
+++ src/routes/address.ts
@@ -4,6 +4,7 @@ import * as os from "os";
 export const addressRouter = Router({})
 
 addressRouter.get('/', (req: Request, res: Response) => {
-    const ipAdr = os.networkInterfaces()
+    // const ipAdr = os.networkInterfaces()
+    const ipAdr = req.ip
     res.send(ipAdr)
 })
