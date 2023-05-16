5542151001a058ce3980514c90ada04abade1707
Oreda-Z <shtucer31@gmail.com>
2023-05-16 15:32:05 +0300

    test ip addresses

--- src/routes/address.ts
+++ src/routes/address.ts
@@ -1,11 +1,28 @@
 import {Router, Request, Response} from "express";
+import {publicIp, publicIpv4, publicIpv6} from 'public-ip';
+import IP from 'ip'
+import {lookup} from "geoip-lite";
 import * as os from "os";
+import {networkInterfaces} from "os";
 
 export const addressRouter = Router({})
 
-addressRouter.get('/', (req: Request, res: Response) => {
-    // const ipAdr = os.networkInterfaces()
-    // const ipAdr = req.ip
-    const ipAdr = req.connection.address()
-    res.send(ipAdr)
+addressRouter.get('/', async (req: Request, res: Response) => {
+    //const ipAdr = os.networkInterfaces()
+    //  const ipAdr = req.ip
+
+    //  const ipAdr = req.ip
+    // //const ipAdr = req.connection.address()
+    // // const ipAddresses = req.header('x-forwarded-for') || req.socket.remoteAddress
+    // //  console.log(ipAddresses)
+    // const ipAddress = IP.address();
+    // // res.send(ipAddress)
+    // const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+    // console.log(lookup(ipAddress))
+    // res.send(lookup(ipAddress))
+    // let interfaces = os.networkInterfaces()
+
+ const helo = publicIp()
+res.send(helo)
+
 })
