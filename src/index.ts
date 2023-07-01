ba3af7e0f6e40f362228eb77f3d28131c41577e3
Oreda-Z <shtucer31@gmail.com>
2023-07-01 13:17:42 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import cookieParser from "cookie-parser";
 
 const port = process.env.PORT || 5000
 
+
 const app = createApp()
+app.set('trust proxy', true)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
--- src/repositories/requestsForApi-db-repository.ts
+++ src/repositories/requestsForApi-db-repository.ts
@@ -8,7 +8,7 @@ export const requestForApiRepository = {
     },
     async findFilteredCounter(IP: string, URL: string, date: Date): Promise<number | null> {
         const newDate = new Date(date.getTime() - 10000);
-        const foundCounters = await requestForApiCollection.find({IP, URL, date: { $gte: newDate}}).toArray()
+        const foundCounters = await requestForApiCollection.find({IP: IP, URL: URL, date: { $gte: newDate}}).toArray()
         // const foundCounters = await requestForApiCollection.find({IP: IP, URL: URL}, {projection: {_id: 0}})
         return foundCounters.length
     },
